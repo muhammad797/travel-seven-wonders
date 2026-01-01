ebf70eb7aff4a29e6adc0c7eec195617a00ba422
Muhammad Ali <muhammad797@hotmail.com>
2026-01-02 00:09:17 +0500

    Favicon updated

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -20,15 +20,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "/favicon.ico",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "/favicon.ico",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "/favicon.ico",
         type: "image/svg+xml",
       },
     ],
